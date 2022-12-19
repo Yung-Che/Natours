7255390a4ee9536cb4accc5b585cba88056ccb3f
Yung-Che <senseye935@gmail.com>
2022-12-19 11:41:29 +0800

    implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,12 +26,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Security HTTP headers
 // app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+app.options('*', cors());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
