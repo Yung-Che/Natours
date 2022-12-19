40ce0fa7e98e0af67f244afaea9fcd9109c37970
Yung-Che <senseye935@gmail.com>
2022-12-19 16:13:57 +0800

    implemented stripe messages

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -39,34 +39,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
   });
-
-  // const session = await stripe.checkout.sessions.create({
-  //   payment_method_types: ['card'],
-  //   success_url: `${req.protocol}://${req.get('host')}/`,
-  //   cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-  //   customer_email: req.user.email,
-  //   client_reference_id: req.params.tourId,
-  //   line_items: [
-  //     {
-  //       quantity: 1,
-  //       price_data: {
-  //         currency: 'usd',
-  //         unit_amount: tour.price * 100,
-  //         product_data: {
-  //           name: `${tour.name} Tour`,
-  //           description: tour.summary,
-  //           images: [
-  //             `${req.protocol}://${req.get('host')}/img/tours/${
-  //               tour.imageCover
-  //             }`,
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   ],
-  //   mode: 'payment',
-  // });
-
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
