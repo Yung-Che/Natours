270e5668d2c7275283d4c55a81e1f1fb2f2bb5b4
Yung-Che <senseye935@gmail.com>
2022-12-19 15:53:37 +0800

    modify strip session

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,10 +15,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
+    mode: 'payment',
     line_items: [
       {
         quantity: 1,
@@ -37,8 +38,34 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         },
       },
     ],
-    mode: 'payment',
   });
+
+  // const session = await stripe.checkout.sessions.create({
+  //   payment_method_types: ['card'],
+  //   success_url: `${req.protocol}://${req.get('host')}/`,
+  //   cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+  //   customer_email: req.user.email,
+  //   client_reference_id: req.params.tourId,
+  //   line_items: [
+  //     {
+  //       quantity: 1,
+  //       price_data: {
+  //         currency: 'usd',
+  //         unit_amount: tour.price * 100,
+  //         product_data: {
+  //           name: `${tour.name} Tour`,
+  //           description: tour.summary,
+  //           images: [
+  //             `${req.protocol}://${req.get('host')}/img/tours/${
+  //               tour.imageCover
+  //             }`,
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   ],
+  //   mode: 'payment',
+  // });
 
   // 3) Create session as response
   res.status(200).json({
@@ -59,7 +86,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
